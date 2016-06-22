7267f4a96643de397868f938c8f6d748e86339e4
cachu <gcachu.o@gmail.com>
2016-06-22 14:59:32 -0500

    diseño

--- App/AppRiego/AppRiego/Form1.Designer.cs
+++ App/AppRiego/AppRiego/Form1.Designer.cs
@@ -48,95 +48,113 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(213, 25);
+            this.label1.Size = new System.Drawing.Size(0, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Final Sustentabilidad";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 120);
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(14, 110);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.Size = new System.Drawing.Size(102, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Hora de riego:";
             // 
             // dtpHoraRegado
             // 
+            this.dtpHoraRegado.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpHoraRegado.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dtpHoraRegado.Location = new System.Drawing.Point(17, 137);
+            this.dtpHoraRegado.Location = new System.Drawing.Point(17, 132);
             this.dtpHoraRegado.Name = "dtpHoraRegado";
-            this.dtpHoraRegado.Size = new System.Drawing.Size(214, 20);
+            this.dtpHoraRegado.Size = new System.Drawing.Size(214, 23);
             this.dtpHoraRegado.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 174);
+            this.label3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(14, 181);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.Size = new System.Drawing.Size(142, 16);
             this.label3.TabIndex = 3;
             this.label3.Text = "Duración de regado:";
             // 
             // txtDuracion
             // 
-            this.txtDuracion.Location = new System.Drawing.Point(17, 191);
+            this.txtDuracion.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDuracion.Location = new System.Drawing.Point(17, 207);
             this.txtDuracion.Name = "txtDuracion";
-            this.txtDuracion.Size = new System.Drawing.Size(214, 20);
+            this.txtDuracion.Size = new System.Drawing.Size(214, 23);
             this.txtDuracion.TabIndex = 4;
             // 
             // btnConfigurar
             // 
-            this.btnConfigurar.Location = new System.Drawing.Point(17, 217);
+            this.btnConfigurar.BackColor = System.Drawing.Color.Black;
+            this.btnConfigurar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfigurar.ForeColor = System.Drawing.Color.White;
+            this.btnConfigurar.Location = new System.Drawing.Point(17, 233);
             this.btnConfigurar.Name = "btnConfigurar";
             this.btnConfigurar.Size = new System.Drawing.Size(208, 46);
             this.btnConfigurar.TabIndex = 5;
             this.btnConfigurar.Text = "Configurar";
-            this.btnConfigurar.UseVisualStyleBackColor = true;
+            this.btnConfigurar.UseVisualStyleBackColor = false;
             this.btnConfigurar.Click += new System.EventHandler(this.btnConfigurar_Click);
             // 
             // btnRegadoManual
             // 
+            this.btnRegadoManual.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.btnRegadoManual.Enabled = false;
-            this.btnRegadoManual.Location = new System.Drawing.Point(17, 301);
+            this.btnRegadoManual.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegadoManual.ForeColor = System.Drawing.Color.White;
+            this.btnRegadoManual.Location = new System.Drawing.Point(17, 317);
             this.btnRegadoManual.Name = "btnRegadoManual";
             this.btnRegadoManual.Size = new System.Drawing.Size(208, 50);
             this.btnRegadoManual.TabIndex = 6;
             this.btnRegadoManual.Text = "Iniciar regado manual";
-            this.btnRegadoManual.UseVisualStyleBackColor = true;
+            this.btnRegadoManual.UseVisualStyleBackColor = false;
             this.btnRegadoManual.Click += new System.EventHandler(this.btnRegadoManual_Click);
             // 
             // txtPuerto
             // 
-            this.txtPuerto.Location = new System.Drawing.Point(17, 66);
+            this.txtPuerto.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPuerto.Location = new System.Drawing.Point(17, 56);
             this.txtPuerto.Name = "txtPuerto";
-            this.txtPuerto.Size = new System.Drawing.Size(120, 20);
+            this.txtPuerto.Size = new System.Drawing.Size(120, 23);
             this.txtPuerto.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(17, 47);
+            this.label4.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(17, 34);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.Size = new System.Drawing.Size(96, 16);
             this.label4.TabIndex = 8;
             this.label4.Text = "Puerto serial:";
             // 
             // btnConectar
             // 
-            this.btnConectar.Location = new System.Drawing.Point(144, 64);
+            this.btnConectar.BackColor = System.Drawing.Color.Green;
+            this.btnConectar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.ForeColor = System.Drawing.Color.White;
+            this.btnConectar.Location = new System.Drawing.Point(144, 54);
             this.btnConectar.Name = "btnConectar";
-            this.btnConectar.Size = new System.Drawing.Size(87, 23);
+            this.btnConectar.Size = new System.Drawing.Size(87, 38);
             this.btnConectar.TabIndex = 9;
             this.btnConectar.Text = "Conectar";
-            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.UseVisualStyleBackColor = false;
             this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(243, 376);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(264, 398);
             this.Controls.Add(this.btnConectar);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtPuerto);
@@ -147,8 +165,10 @@
             this.Controls.Add(this.dtpHoraRegado);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "App Riego";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jardín Inteligente";
             ((System.ComponentModel.ISupportInitialize)(this.txtDuracion)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtPuerto)).EndInit();
             this.ResumeLayout(false);
